f38e0815f7ccd996e2714bea1753c8ca4e2d6547
Shaun Roos <51285594+ShaunRoos@users.noreply.github.com>
2019-07-22 20:58:42 +0200

    Add files via upload

---
+++ Project_Euler_Question_04/Project_Euler_Question_04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler_Question_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> palindrome = new List<int>();
+            int value;
+            char[] revpalindrome;
+
+            for(int i = 999; i > 99; i--)
+            {
+                for(int j = 999; j > 99; j--)
+                {
+                    value = i * j;
+                    revpalindrome = value.ToString().ToCharArray();
+                    Array.Reverse(revpalindrome);
+
+                    if(value == int.Parse(new string(revpalindrome)))
+                    {
+                        palindrome.Add(value);
+                    }
+                        
+                }
+            }
+            Console.WriteLine("The largest palindromic number is: " + palindrome.Max());
+            Console.ReadLine();
+        }
+    }
+}
